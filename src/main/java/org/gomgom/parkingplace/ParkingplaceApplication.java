f41b1e8c369ae7b44a134b6ab8370440592088a2
rjsah09 <rjsah09@naver.com>
2024-09-02 11:42:30 +0900

    first commit

---
+++ src/main/java/org/gomgom/parkingplace/ParkingplaceApplication.java
@@ -0,0 +1,13 @@
+package org.gomgom.parkingplace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParkingplaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParkingplaceApplication.class, args);
+	}
+
+}
