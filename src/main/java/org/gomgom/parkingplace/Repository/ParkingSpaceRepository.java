31466fddeab3d31ab202120dac9ee85e45cb5ea6
gaengmin <momo5736@naver.com>
2024-09-04 16:42:23 +0900

    Feat : 다른 서비스단 생성
    Feat : 다른 서비스단 생성

--- src/main/java/org/gomgom/parkingplace/Repository/ParkingSpaceRepository.java
+++ src/main/java/org/gomgom/parkingplace/Repository/ParkingSpaceRepository.java
@@ -2,6 +2,11 @@ package org.gomgom.parkingplace.Repository;
 
 import org.gomgom.parkingplace.Entity.ParkingSpace;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 /**
  * @Author : 김경민
@@ -10,4 +15,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
  * */
 public interface ParkingSpaceRepository extends JpaRepository<ParkingSpace, Long> {
 
+    @Query("SELECT ps FROM ParkingSpace ps LEFT JOIN Reservation r " +
+            "ON ps.id = r.parkingSpace.id " +
+            "AND r.startTime < :endTime AND r.endTime > :startTime " +
+            "WHERE ps.parkingLot.id = :parkingLotId AND r.id IS NULL")
+    List<ParkingSpace> findAvailableSpaces(@Param("parkingLotId") Long parkingLotId,
+                                           @Param("startTime") LocalDateTime startTime,
+                                           @Param("endTime") LocalDateTime endTime);
 }
