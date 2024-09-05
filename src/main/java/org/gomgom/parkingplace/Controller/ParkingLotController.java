8c5fac1037611479a739482f8505c3860b551b03
rjsah09 <rjsah09@naver.com>
2024-09-05 19:13:55 +0900

    Feat: 위도, 경도 범위 내 마커 생성용 주차장 목록 api

--- src/main/java/org/gomgom/parkingplace/Controller/ParkingLotController.java
+++ src/main/java/org/gomgom/parkingplace/Controller/ParkingLotController.java
@@ -8,13 +8,6 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-/*
- * 생성 일자: 2024.09.05
- * 설명 : 주차장 관련 api 컨트롤러
- * ---------------------
- * 2024.09.05 양건모 | getParkingLots(): 특정 위도 경도 범위 내의 주차장 목록 조회
- *
- * */
 
 @RestController
 @RequiredArgsConstructor
@@ -23,7 +16,15 @@ public class ParkingLotController {
 
     private final ParkingLotService parkingLotService;
 
-    //특정 위도 경도 범위 내의 주차장 목록 조회
+    /**
+     * 작성자: 양건모
+     * 시작 일자: 2024.09.02
+     * 설명 : 위도 경도 범위 내의 주차장들의 위도, 경도를 포함한 정보 제공
+     * @param request 최소 위도, 최대 위도, 최소 경도, 최대 경도
+     * @return List 형태로 id, 이름, 위도, 경도, 주소
+     *  ---------------------
+     * 2024.09.03 양건모 | 기능 구현
+     * */
     @GetMapping
     public ParkingLotDto.ParkingLotMarkersDto getParkingLots(
             @ModelAttribute ParkingLotDto.ParkingLotListRequestDto request) {
--- src/main/java/org/gomgom/parkingplace/Service/parkingLot/ParkingLotService.java
+++ src/main/java/org/gomgom/parkingplace/Service/parkingLot/ParkingLotService.java
@@ -1,16 +1,7 @@
 package org.gomgom.parkingplace.Service.parkingLot;
 
-
 import org.gomgom.parkingplace.Dto.ParkingLotDto;
 
-import java.util.List;
-
-/**
- * 생성 일자: 2024.09.05
- * 설명 : 주차장 관련 서비스
- * ---------------------
- * 2024.09.05 양건모 | getParkingLots(): 특정 위도 경도 범위 내의 주차장 목록 조회
- * */
 
 public interface ParkingLotService {
 
