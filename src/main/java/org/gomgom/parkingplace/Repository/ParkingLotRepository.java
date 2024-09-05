52768e9e9014c8b45930ba0b5de9118b20488e66
rjsah09 <rjsah09@naver.com>
2024-09-05 17:27:45 +0900

    Feat: ParkingSpaceController, ParkingSpaceService 생성

--- src/main/java/org/gomgom/parkingplace/Repository/ParkingLotRepository.java
+++ src/main/java/org/gomgom/parkingplace/Repository/ParkingLotRepository.java
@@ -1,9 +1,13 @@
 package org.gomgom.parkingplace.Repository;
 
 
+import org.gomgom.parkingplace.Dto.ParkingLotDto;
 import org.gomgom.parkingplace.Entity.ParkingLot;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import java.util.List;
 import java.util.Optional;
 
 /**
@@ -13,4 +17,13 @@ import java.util.Optional;
  */
 public interface ParkingLotRepository extends JpaRepository<ParkingLot, Long> {
 
+    @Query("SELECT new org.gomgom.parkingplace.Dto.ParkingLotDto$ParkingLotMarkerDto(pl)" +
+            "FROM ParkingLot pl " +
+            "WHERE pl.latitude BETWEEN :minLat AND :maxLat " +
+            "AND pl.longitude BETWEEN :minLon AND :maxLon")
+    List<ParkingLotDto.ParkingLotMarkerDto> getParkingLots(
+            @Param("minLat") double minLat,
+            @Param("maxLat") double maxLat,
+            @Param("minLon") double minLon,
+            @Param("maxLon") double maxLon);
 }
