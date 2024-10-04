ef72a81d960a5e0c1dfbde84176492be79f00b51
rjsah09 <rjsah09@naver.com>
2024-10-04 14:00:04 +0900

    Feat: 타임존 서울로 변경

--- src/main/java/org/gomgom/parkingplace/ParkingplaceApplication.java
+++ src/main/java/org/gomgom/parkingplace/ParkingplaceApplication.java
@@ -5,6 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+import java.util.TimeZone;
+
 /**
  * 스케쥴러 활성화
  * 2024.09.11
@@ -16,6 +18,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 public class ParkingplaceApplication {
 
 	public static void main(String[] args) {
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
 		SpringApplication.run(ParkingplaceApplication.class, args);
 	}
 
