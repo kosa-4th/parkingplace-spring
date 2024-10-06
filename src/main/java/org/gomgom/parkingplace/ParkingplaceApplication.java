8e9dace74c893836d409fe46cc0694ae845dda72
rjsah09 <rjsah09@naver.com>
2024-10-06 14:43:21 +0900

    Feat: 시작 시간 로그 추가

--- src/main/java/org/gomgom/parkingplace/ParkingplaceApplication.java
+++ src/main/java/org/gomgom/parkingplace/ParkingplaceApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+import java.time.LocalDateTime;
 import java.util.TimeZone;
 
 /**
@@ -19,6 +20,7 @@ public class ParkingplaceApplication {
 
 	public static void main(String[] args) {
 		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+		System.out.println("Started At " + LocalDateTime.now());
 		SpringApplication.run(ParkingplaceApplication.class, args);
 	}
 
